df0a7eef73ebf4b4f6d9134a732d0904fbfbf636
Chris <condottiero1985@gmail.com>
2022-01-10 20:27:18 +0100

    Add missing Core project

---
+++ Source/Core/Dtos/Day.cs
@@ -0,0 +1,22 @@
+﻿namespace Dtos
+{
+    public sealed class Day
+    {
+        public DateTime? Start { get; set; }
+
+        public DateTime? End { get; set; }
+
+        public decimal? Time { get; set; }
+
+        public decimal? Break { get; set; }
+
+        public override string ToString()
+        {
+            var start = Start.HasValue ? Start.Value.ToShortDateString() : string.Empty;
+            var startTime = Start.HasValue ? Start.Value.ToShortTimeString() : string.Empty;
+            var end = End.HasValue ? End.Value.ToShortTimeString() : "<no end found.>";
+            var workTime = Time.HasValue ? Time.ToString() : "0.0";
+            return $"{start} - {startTime} - {end} - {workTime}";
+        }
+    }
+}
---
+++ Source/Core/Logging/Logger.cs
@@ -0,0 +1,23 @@
+﻿using Core.Configuration;
+
+namespace Core.Logging
+{
+    public class Logger : ILogger
+    {
+        readonly Paths _path;
+
+        public Logger(Paths path)
+        {
+            _path = path;
+        }
+
+        public void Error(Exception exception)
+        {
+            try
+            {
+                File.AppendAllText(_path.Log, exception.ToString());
+            }
+            catch { }
+        }
+    }
+}
