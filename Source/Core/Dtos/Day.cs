3d29cbc46a16287f71377b04ba9ee82ed25da4f2
ms-sk <39585579+ms-sk@users.noreply.github.com>
2022-01-12 15:17:27 +0100

    #3 refactoring

--- Source/Core/Dtos/Day.cs
+++ Source/Core/Dtos/Day.cs
@@ -9,14 +9,5 @@
         public decimal? Time { get; set; }
 
         public decimal? Break { get; set; }
-
-        public override string ToString()
-        {
-            var start = Start.HasValue ? Start.Value.ToShortDateString() : string.Empty;
-            var startTime = Start.HasValue ? Start.Value.ToShortTimeString() : string.Empty;
-            var end = End.HasValue ? End.Value.ToShortTimeString() : "<no end found.>";
-            var workTime = Time.HasValue ? Time.ToString() : "0.0";
-            return $"{start} - {startTime} - {end} - {workTime}";
-        }
     }
 }
--- Source/WorkTimeTracker/ViewModels/FilterViewModel.cs
+++ Source/WorkTimeTracker/ViewModels/FilterViewModel.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Input;
 
 namespace WorkTimeTracker.ViewModels
 {
