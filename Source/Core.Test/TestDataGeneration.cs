afaa54085ee15f7079cb1110bb9e913fd883b328
Chris <condottiero1985@gmail.com>
2022-02-15 22:21:27 +0100

    extend test data generation

--- Source/Core.Test/TestDataGeneration.cs
+++ Source/Core.Test/TestDataGeneration.cs
@@ -10,7 +10,6 @@ namespace Core.Test
     public class TestDataGeneration
     {
         [TestMethod]
-        [Ignore]
         public void GenerateWorkTimesTestData()
         {
             var worktimeStorage = new WorkTimeStorage(new Paths());
@@ -18,11 +17,30 @@ namespace Core.Test
             var worktime = new WorkTime();
             var date = new DateTime(2021, 12, 1);
             var rnd = new Random();
-            while(date <= DateTime.Today)
+            while (date <= DateTime.Today)
             {
                 var start = date.AddMinutes(rnd.Next(360, 480));
+                var end = start.AddMinutes(rnd.Next(360, 540));
 
-                worktime.Days.Add(new Day { Start = start, End = null, Break = 0, Time = 0 });
+                var hours = (end - start).TotalHours;
+                var rounded = Math.Round(hours * 4, MidpointRounding.ToEven) / 4.0;
+
+                // TODO Fix and optimize break calculation
+                var @break = 0.0;
+                if (hours <= 6.0)
+                {
+                    @break = 0.0;
+                }
+                else if (hours <= 9.0)
+                {
+                    @break = 0.5;
+                }
+                else
+                {
+                    @break = 1;
+                }
+
+                worktime.Days.Add(new Day { Start = start, End = end, Time = (decimal)rounded, Break = (decimal)@break });
 
                 date = date.AddDays(1);
             }
