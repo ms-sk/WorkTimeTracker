f8b4200f1c4d40382e3c603e1d770410cc21c333
ms-sk <39585579+ms-sk@users.noreply.github.com>
2022-02-23 15:51:19 +0100

    Add core button

---
+++ Source/Core.Wpf/Controls/CButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Core.Wpf.Controls
+{
+    public sealed class CButton : Button
+    {
+        static CButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CButton), new FrameworkPropertyMetadata(typeof(CButton)));
+        }
+
+        public CButton()
+        {
+            Background = new SolidColorBrush(Colors.White);
+        }
+
+        public static readonly DependencyProperty IconProperty =
+            DependencyProperty.Register(nameof(Icon), typeof(object), typeof(CButton), new PropertyMetadata(null));
+
+        public object Icon
+        {
+            get { return GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+    }
+}
---
+++ Source/Core.Wpf/Controls/CSeparator.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Core.Wpf.Controls
+{
+    public sealed class CSeparator : Separator
+    {
+        static CSeparator()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CSeparator), new FrameworkPropertyMetadata(typeof(CSeparator)));
+        }
+    }
+}
