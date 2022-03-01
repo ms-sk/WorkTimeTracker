8c4ce267e4b7cb4418c50c4a90f01296aa40ae9f
Chris <condottiero1985@gmail.com>
2022-03-01 22:42:49 +0100

    Set default settings

--- Source/Core/Dtos/Settings.cs
+++ Source/Core/Dtos/Settings.cs
@@ -4,6 +4,12 @@ namespace Core.Dtos
 {
     public sealed class Settings
     {
+        public Settings()
+        {
+            Filter = Filter.None;
+            HoursPerDay = 8.0;
+        }
+
         public Filter Filter { get; set; }
 
         public double HoursPerDay { get; set; }
